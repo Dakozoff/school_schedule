d909c80d7c4d3fb6f2a7c8c9f1eddb34e8909e01
Dakozoff <109003953+Dakozoff@users.noreply.github.com>
2024-05-14 18:20:34 +0300

    Add files via upload

--- school_schedule/utils/console.py
+++ school_schedule/utils/console.py
@@ -6,39 +6,66 @@ class Console:
         self.schedule = Schedule(database)
 
     def run(self):
-        while True:
-            action = input("Введите действие, доступные: (add_teacher, add_subject, add_classroom, add_schedule_item, view_schedule, delete_schedule_item, exit): ")
+        start_text = ("""
+School Schedule - управление расписанием учебного заведения
 
-            if action == "add_teacher":
+Управление:
+1. Добавить учителя
+2. Добавить предмет
+3. Добавить класс
+4. Добавить элемент в расписание
+5. Посмотреть расписание
+6. Удалить элемент расписания
+
+""")
+
+        print(start_text)
+
+        while True:
+            action = int(input("Введите запрос: "))
+            
+            if action == 1:
+                print("~~~~")
                 name = input("Введите имя учителя: ")
                 self.schedule.add_teacher(name)
+                print("~~~~")
                 
-            elif action == "add_subject":
+            elif action == 2:
+                print("~~~~")
                 name = input("Введите название предмета: ")
                 self.schedule.add_subject(name)
+                print("~~~~")
 
-            elif action == "add_classroom":
+            elif action == 3:
+                print("~~~~")
                 number = int(input("Введите номер класса: "))
                 capacity = int(input("Введите вместимость класса: "))
                 self.schedule.add_classroom(number, capacity)
+                print("~~~~")
 
-            elif action == "add_schedule_item":
+            elif action == 4:
+                print("~~~~")
                 subject_name = input("Введите название предмета: ")
                 teacher_name = input("Введите имя учителя: ")
                 classroom_number = int(input("Введите номер класса: "))
                 self.schedule.add_schedule_item(subject_name, teacher_name, classroom_number)
+                print("~~~~")
 
-            elif action == "view_schedule":
+            elif action == 5:
+                print("~~~~")
                 self.schedule.view_schedule()
+                print("~~~~")
 
-            elif action == "delete_schedule_item":
+            elif action == 6:
+                print("~~~~")
                 subject_name = input("Введите название предмета: ")
                 teacher_name = input("Введите имя учителя: ")
                 self.schedule.delete_schedule_item(subject_name, teacher_name)
+                print("~~~~")
 
             elif action == "exit":
                 self.db.close()
                 break
 
             else:
-                print("Действие не найдено. Список доступных: add_teacher, add_subject, add_classroom, add_schedule_item, view_schedule, delete_schedule_item, exit")
+                print("Действие не найдено.")
